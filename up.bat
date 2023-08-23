239d623d36bd13f7ca7473d10591ab7309e39543
CodeWracker <dev.rodrigofs@gmail.com>
2023-08-23 17:25:11 -0300

    o ./up.bat funciona pra dar up em qualquer computador agora

--- up.bat
+++ up.bat
@@ -1,3 +1,11 @@
 docker-compose up -d
-docker exec -it aso-docker-image-ubuntu-aso-1 /bin/bash
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+FOR /F "tokens=1" %%i IN ('docker-compose ps ^| findstr "ubuntu-aso"') DO (
+    set "containerName=%%i"
+    goto :breakLoop
+)
+:breakLoop
+docker exec -it %containerName% /bin/bash
 docker-compose down
